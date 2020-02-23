6776089f9edd3634030d26d5bb0ec31930a9827e
Ogonnaya <hello@ogonnaya.com>
2020-02-23 10:55:06 -0600

    added navigation and linked landing, exclusive vendors and become a vendor pages

---
+++ project-3/client/src/pages/planners/signup.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function signup() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Become a Vendor</p>
+      </header>
+    </div>
+  );
+}
+
+export default signup;
