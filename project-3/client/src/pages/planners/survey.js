0e35e238636da488f6426d34bbc31aacbbbc4abb
Edward Martey <martey8709@gmail.com>
2020-02-25 19:43:59 -0600

    Survey working

--- project-3/client/src/pages/planners/survey.js
+++ project-3/client/src/pages/planners/survey.js
@@ -1,185 +1,200 @@
-import React from "react";
-//Add a property a text property into all questions types and into page
-Survey
-    .Serializer
-    .addProperty("question", "popupdescription:text");
-Survey
-    .Serializer
-    .addProperty("page", "popupdescription:text");
-function showDescription(element) {
-    document
-        .getElementById("questionDescriptionText")
-        .innerHTML = element.popupdescription;
-    $("#questionDescriptionPopup").modal();
-}
 
-Survey
-    .StylesManager
-    .applyTheme("modern");
+// //Add a property a text property into all questions types and into page
+// Survey
+//     .Serializer
+//     .addProperty("question", "popupdescription:text");
+// Survey
+//     .Serializer
+//     .addProperty("page", "popupdescription:text");
+// function showDescription(element) {
+//     document
+//         .getElementById("questionDescriptionText")
+//         .innerHTML = element.popupdescription;
+//     $("#questionDescriptionPopup").modal();
+// }
 
-var json = {
-    title: "Event Planner Survey.",
-    pages: [
-        {
-            title: "What type of event are you planning?",
-            popupdescription: "Select event type you plan on having?",
-            questions: [
-                {
-                    type: "checkbox",
-                    name: "Type",
-                    title: "Event",
-                    hasOther: true,
-                    isRequired: true,
-                    popupdescription: "Select event type closest to description of desired event",
-                    choices: ["Party (Birthday, Sweet 16, Quinceañera, Bar Mitzvah, etc.)", "Wedding", "Charity", "Corporate", "Funeral"]
-                }
-            ]
-        }, {
-            title: "What type of vendor(s) do you need for your event?",
-            popupdescription: "Select all programming languages you have been using for the last six months.",
-            questions: [
-                {
-                    type: "checkbox",
-                    name: "Vendors",
-                    title: "Please select ALL that apply from the list",
-                    popupdescription: "Select vendor(s) you will need for the event",
-                    isRequired: true,
-                    choices: [
-                        "Event Planner", "Decorator", "Caterer", "Photographer", "DJ", "Host"
-                    ]
-                }
-            ]
-        }, {
-            title: "What locale would you like to have your event?",
-            popupdescription: "We will not share this information with any third-party organization.",
-            questions: [
-                {
-                    type: "dropdown",
-                    name: "State",
-                    title: "State you would like to have event",
-                    popupdescription: "Select state you wish to hold your event",
-                    isRequired: true,
-                    choices: [
-                        'Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Federated States of Micronesia', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-                    ]
-                }, {
-                    type: "text",
-                    name: "city",
-                    title: "Name of city you would like to have event",
-                    popupdescription: "Please, make sure you do not misspell the city name"
-                }
-            ]
-        }
-    ]
-};
+// Survey
+//     .StylesManager
+//     .applyTheme("modern");
 
-window.survey = new Survey.Model(json);
+// var json = {
+//     title: "Event Planner Survey.",
+//     pages: [
+//         {
+//             "elements": [
+//                 {
+//                     "type": "panel",
+//                     "elements": [
+//                         {
+//                             "type": "html",
+//                             "name": "income_intro",
+//                             "html": "<article class='intro'>    <h1 class='intro__heading intro__heading--income title'>                     Kollab Your Next Event!              </h1>    <div class='intro__body wysiwyg'>       <p>In this section, you will be asked some of the details of your event to put your optimal vendor team together. Let's KOLLAB!</p>  </div> </article>"
+//                         }
+//                     ],
+//                     "name": "panel1"
+//                 }
+//             ],
+//             "name": "page0"
+//         },
 
-survey
-    .onComplete
-    .add(function (result) {
-        document
-            .querySelector('#surveyResult')
-            .textContent = "Result JSON:\n" + JSON.stringify(result.data, null, 3);
-    });
+//         {
+//             title: "What type of event are you planning?",
+//             popupdescription: "Select event type you plan on having?",
+//             questions: [
+//                 {
+//                     type: "checkbox",
+//                     name: "Type",
+//                     title: "Event",
+//                     hasOther: true,
+//                     isRequired: true,
+//                     popupdescription: "Select event type closest to description of desired event",
+//                     choices: ["Party (Birthday, Sweet 16, Quinceañera, Bar Mitzvah, etc.)", "Wedding", "Charity", "Corporate", "Funeral"]
+//                 }
+//             ]
+//         }, {
+//             title: "What type of vendor(s) do you need for your event?",
+//             popupdescription: "Select all programming languages you have been using for the last six months.",
+//             questions: [
+//                 {
+//                     type: "checkbox",
+//                     name: "Vendors",
+//                     title: "Please select ALL that apply from the list",
+//                     popupdescription: "Select vendor(s) you will need for the event",
+//                     isRequired: true,
+//                     choices: [
+//                         "Event Planner", "Decorator", "Caterer", "Photographer", "DJ", "Host"
+//                     ]
+//                 }
+//             ]
+//         }, {
+//             title: "What locale would you like to have your event?",
+//             popupdescription: "We will not share this information with any third-party organization.",
+//             questions: [
+//                 {
+//                     type: "dropdown",
+//                     name: "State",
+//                     title: "State you would like to have event",
+//                     popupdescription: "Select state you wish to hold your event",
+//                     isRequired: true,
+//                     choices: [
+//                         'Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Federated States of Micronesia', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+//                     ]
+//                 }, {
+//                     type: "text",
+//                     name: "City",
+//                     title: "Name of city you would like to have event",
+//                     popupdescription: "Please, make sure you do not misspell the city name"
+//                 }
+//             ]
+//         }
+//     ]
+// };
 
-survey
-    .onAfterRenderQuestion
-    .add(function (survey, options) {
-        //Return if there is no description to show in popup
-        if (!options.question.popupdescription)
-            return;
+// window.survey = new Survey.Model(json);
 
-        //Add a button;
-        var btn = document.createElement("button");
-        btn.type = "button";
-        btn.className = "btn btn-info btn-xs";
+// survey
+//     .onComplete
+//     .add(function (result) {
+//         document
+//             .querySelector('#surveyResult')
+//             .textContent = "Result JSON:\n" + JSON.stringify(result.data, null, 3);
+//     });
 
-        btn.style.position = "absolute";
-        btn.style.marginLeft = "20px"
+// survey
+//     .onAfterRenderQuestion
+//     .add(function (survey, options) {
+//         //Return if there is no description to show in popup
+//         if (!options.question.popupdescription)
+//             return;
 
-        btn.innerHTML = "More Info";
-        var question = options.question;
-        btn.onclick = function () {
-            showDescription(question);
-        }
-        var header = options
-            .htmlElement
-            .querySelector("h5");
-        var span = document.createElement("span");
-        span.innerHTML = "  ";
-        header.appendChild(span);
-        header.appendChild(btn);
-    });
+//         //Add a button;
+//         var btn = document.createElement("button");
+//         btn.type = "button";
+//         btn.className = "btn btn-info btn-xs";
 
-survey
-    .onAfterRenderPage
-    .add(function (survey, options) {
-        //Return if there is no description to show in popup
-        if (!options.page.popupdescription)
-            return;
+//         btn.style.position = "absolute";
+//         btn.style.marginLeft = "20px"
 
-        //Add a button;
-        var btn = document.createElement("button");
-        btn.type = "button";
-        btn.className = "btn btn-info btn-xs";
+//         btn.innerHTML = "More Info";
+//         var question = options.question;
+//         btn.onclick = function () {
+//             showDescription(question);
+//         }
+//         var header = options
+//             .htmlElement
+//             .querySelector("h5");
+//         var span = document.createElement("span");
+//         span.innerHTML = "  ";
+//         header.appendChild(span);
+//         header.appendChild(btn);
+//     });
 
-        btn.style.position = "absolute";
-        btn.style.marginLeft = "20px"
+// survey
+//     .onAfterRenderPage
+//     .add(function (survey, options) {
+//         //Return if there is no description to show in popup
+//         if (!options.page.popupdescription)
+//             return;
 
-        btn.innerHTML = "More Info";
-        btn.onclick = function () {
-            showDescription(survey.currentPage);
-        }
-        var header = options
-            .htmlElement
-            .querySelector("h4");
-        var span = document.createElement("span");
-        span.innerHTML = "  ";
-        header.appendChild(span);
-        header.appendChild(btn);
-    });
-ReactDOM.render(<Survey.Survey model={survey} />, document.getElementById("surveyElement"));
+//         //Add a button;
+//         var btn = document.createElement("button");
+//         btn.type = "button";
+//         btn.className = "btn btn-info btn-xs";
 
-function animate(animitionType, duration) {
-    if (!duration)
-        duration = 1000;
-    var element = document.getElementById("surveyElement");
-    $(element).velocity(animitionType, { duration: duration });
-}
+//         btn.style.position = "absolute";
+//         btn.style.marginLeft = "20px"
 
-var doAnimantion = true;
-survey
-    .onCurrentPageChanging
-    .add(function (sender, options) {
-        if (!doAnimantion)
-            return;
-        options.allowChanging = false;
-        setTimeout(function () {
-            doAnimantion = false;
-            sender.currentPage = options.newCurrentPage;
-            doAnimantion = true;
-        }, 500);
-        animate("slideUp", 500);
-    });
-survey
-    .onCurrentPageChanged
-    .add(function (sender) {
-        animate("slideDown", 500);
-    });
-survey
-    .onCompleting
-    .add(function (sender, options) {
-        if (!doAnimantion)
-            return;
-        options.allowComplete = false;
-        setTimeout(function () {
-            doAnimantion = false;
-            sender.doComplete();
-            doAnimantion = true;
-        }, 500);
-        animate("slideUp", 500);
-    });
-animate("slideDown", 1000);
+//         btn.innerHTML = "More Info";
+//         btn.onclick = function () {
+//             showDescription(survey.currentPage);
+//         }
+//         var header = options
+//             .htmlElement
+//             .querySelector("h4");
+//         var span = document.createElement("span");
+//         span.innerHTML = "  ";
+//         header.appendChild(span);
+//         header.appendChild(btn);
+//     });
+// ReactDOM.render(<Survey.Survey model={survey} />, document.getElementById("surveyElement"));
 
-export default survey;
+// function animate(animitionType, duration) {
+//     if (!duration)
+//         duration = 1000;
+//     var element = document.getElementById("surveyElement");
+//     $(element).velocity(animitionType, { duration: duration });
+// }
+
+// var doAnimantion = true;
+// survey
+//     .onCurrentPageChanging
+//     .add(function (sender, options) {
+//         if (!doAnimantion)
+//             return;
+//         options.allowChanging = false;
+//         setTimeout(function () {
+//             doAnimantion = false;
+//             sender.currentPage = options.newCurrentPage;
+//             doAnimantion = true;
+//         }, 500);
+//         animate("slideUp", 500);
+//     });
+// survey
+//     .onCurrentPageChanged
+//     .add(function (sender) {
+//         animate("slideDown", 500);
+//     });
+// survey
+//     .onCompleting
+//     .add(function (sender, options) {
+//         if (!doAnimantion)
+//             return;
+//         options.allowComplete = false;
+//         setTimeout(function () {
+//             doAnimantion = false;
+//             sender.doComplete();
+//             doAnimantion = true;
+//         }, 500);
+//         animate("slideUp", 500);
+//     });
+// animate("slideDown", 1000);
