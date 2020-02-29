3fb3af01a07fd48b33e1aba811fcba5aee9dd138
Jhayla Young <jhayla.young@gmail.com>
2020-02-29 09:44:41 -0600

    trying to get it to work

--- project-3/client/src/App.js
+++ project-3/client/src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
-<<<<<<< HEAD
 import { motion } from "framer-motion"
-=======
->>>>>>> fac4637c5a34262ec14607f2e131a2babd3702ab
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from "./components/Nav/Header";
+import Header from "./components/Header/Header";
 import index from "./pages/index";
 import features from "./pages/planners/features";
 import signup from "./pages/planners/signup";
-
 
 function App() {
   return (
@@ -16,21 +13,9 @@ function App() {
       <div>
         <Header />
         <Switch>
-<<<<<<< HEAD
-<<<<<<< HEAD
           <Route exact path="/" component={index} />
           <Route exact path="/planners/features" component={features} />
           <Route exact path="/planners/signup" component={signup} />
-=======
-        <Route exact path="/" component={index} />
-        <Route exact path="/planners/features" component={features} />
-        <Route exact path="/planners/signup" component={signup} />
->>>>>>> fac4637c5a34262ec14607f2e131a2babd3702ab
-=======
-          <Route exact path="/" component={index} />
-          <Route exact path="/planners/features" component={features} />
-          <Route exact path="/planners/signup" component={signup} />
->>>>>>> 0e35e238636da488f6426d34bbc31aacbbbc4abb
         </Switch>
       </div>
     </Router>
