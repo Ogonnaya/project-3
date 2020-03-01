c3825b81870175c01cd0cb5c8df5d0a2b4f305e7
Andre <andre.morrall@icloud.com>
2020-03-01 09:51:58 -0600

    Add database seed file, update App.js to render profile page

---
+++ project-3/client/src/pages/planners/profile.js
@@ -0,0 +1,14 @@
+// import React, { Component } from "react";
+import React from "react";
+
+function profile() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Vendor profile</p>
+            </header>
+        </div>
+    );
+}
+
+export default profile;
