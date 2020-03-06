77ac951b97d592cea87ee4205840d70d98183d27
Ogonnaya <hello@ogonnaya.com>
2020-03-05 20:40:23 -0600

    added build folder

---
+++ client/build/precache-manifest.271dfeef63ff3db6bf8c76391aeb1aa1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d2e0396659a63b8dd0719f05ee512af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87cb21cd49986a76848c",
+    "url": "/static/css/2.9c735e37.chunk.css"
+  },
+  {
+    "revision": "5f839d328c66dbc6d3ac",
+    "url": "/static/css/main.c373dede.chunk.css"
+  },
+  {
+    "revision": "87cb21cd49986a76848c",
+    "url": "/static/js/2.c5f94e38.chunk.js"
+  },
+  {
+    "revision": "c7890d60ddd24f3515ac134281d06887",
+    "url": "/static/js/2.c5f94e38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f839d328c66dbc6d3ac",
+    "url": "/static/js/main.9068c5a7.chunk.js"
+  },
+  {
+    "revision": "f73a1e47ef8f34caa41a",
+    "url": "/static/js/runtime-main.a67ebcde.js"
+  },
+  {
+    "revision": "47c23d6bdf766c735c6e986b9b947842",
+    "url": "/static/media/Project_3_Background.47c23d6b.png"
+  }
+]);
